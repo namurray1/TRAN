b8471bc6526490ce14bceadaab8da2d6a66fd825
Melinda Owen <Melinda Owen>
2017-05-22 22:55:10 -0400

    working on passport/login/auth

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var logger = require("morgan");
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 var path = require("path");
+var passport = require("passport");
+var flash = require("connect-flash");
+require('./config/passport')(passport); // pass passport for configuration
 
 // Require History Schema Create Instance of Express
 var app = express();
@@ -14,31 +17,42 @@ var PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 var db = require("./models");
 
+// set up ejs for templating
+app.set('view engine', 'ejs'); 
+
 // Run Morgan for Logging
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(cookieParser()); // read cookies (needed for auth)
 
-// After the body is parsed, it's time for validation this starts the express
-// validator
-app.use(expressValidator());
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-//Express Session
-app.use(expressSession({
-    secret: 'secret code',
-    // If saveUnitialized is set to true it will save a session to our session
-    // storage even if it is not initialized
-    saveUninitialized: false,
-    // If resave is set to true it will save our session after each request false
-    // will only save if we change something
-    resave: false
-}));
+// - Mel - commenting this out for now while I work out passportJS to use instead of this
+// //Express Session
+// app.use(expressSession({
+//     secret: 'secret code',
+//     // If saveUnitialized is set to true it will save a session to our session
+//     // storage even if it is not initialized
+//     saveUninitialized: false,
+//     // If resave is set to true it will save our session after each request false
+//     // will only save if we change something
+//     resave: false
+// }));
 
-require("./controllers/html-routes.js")(app);
+// required for passport
+app.use(session({ secret: 'superhero' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require("./controllers/html-routes.js")(app,passport);
 require("./controllers/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
